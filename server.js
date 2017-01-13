cd63c95d5949a1a6aae1104aa130eb258781803a
aidanbdh <aidanbdh1998@gmail.com>
2017-01-13 13:51:57 -0800

    Heroku psql db implementation #8

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pg = require('')
 const knex = require('knex')({
   client: 'pg',
   connection:'//xuapadzjztxhns:5a36040cc5803628188a06fc5fafee4358fed714559c00df6ef8037833ea456e@ec2-50-17-220-223.compute-1.amazonaws.com:5432/dc39hfq2kphpm9',
