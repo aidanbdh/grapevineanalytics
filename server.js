dd81983a0fe9bee26bc041b1c0de405f67ce4f11
aidanbdh <aidanbdh1998@gmail.com>
2017-01-16 16:47:38 -0800

    Changed configuration for deployment#8

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const knex = require('knex')({
     client: 'pg',
     //Testing
-    connection: { database: 'grapevineanalytics' }
+    //connection: { database: 'grapevineanalytics' }
     //Implementation
-    /*connection: 'postgres://xuapadzjztxhns:5a36040cc5803628188a06fc5fafee4358fed714559c00df6ef8037833ea456e@ec2-50-17-220-223.compute-1.amazonaws.com:5432/dc39hfq2kphpm9',
-    ssl: true*/
+    connection: 'postgres://xuapadzjztxhns:5a36040cc5803628188a06fc5fafee4358fed714559c00df6ef8037833ea456e@ec2-50-17-220-223.compute-1.amazonaws.com:5432/dc39hfq2kphpm9',
+    ssl: true
   });
 
 const app = express();
