3fee38154d0aec2125a45033255b6066dac9c741
aidanbdh <aidanbdh1998@gmail.com>
2017-01-17 16:56:55 -0800

    Nightly commit #19

--- create-tables.sql
+++ create-tables.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS analytics_1;
+DROP TABLE IF EXISTS analytics_1.1;
+DROP TABLE IF EXISTS analytics_1.2;
+DROP TABLE IF EXISTS analytics_1.3;
 DROP TABLE IF EXISTS analytics_2;
 DROP TABLE IF EXISTS analytics_3;
 
